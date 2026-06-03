eca8ba1ff12dfa9267574006dfbcbd6d9f7818dd
arielbetancor87-cmyk <ariel.betancor87@gmail.com>
2026-06-03 17:15:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: "public",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true,
     commonjsOptions: { transformMixedEsModules: true }
   },
